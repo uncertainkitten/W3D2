e7387b4a4bf3c3dc92f172c4eb5a9cb0f1535be2
Cassandra <uncertainkitten@gmail.com>
2018-09-04 16:31:43 -0700

    Finish aaquestions medium level - add utility methods to User and Question for followers

--- aa_questions/aaquestions.rb
+++ aa_questions/aaquestions.rb
@@ -61,6 +61,10 @@ class Question
   def replies
     Reply.find_by_question_id(self.id)
   end
+  
+  def followers
+    QuestionFollow.followers_for_question_id(self.id)
+  end
 end
 
 class Reply
@@ -190,6 +194,10 @@ class User
   def authored_replies
     Reply.find_by_user_id(self.id)
   end 
+  
+  def followed_questions
+    QuestionFollow.followed_questions_for_user_id(self.id)
+  end
 end
 
 class QuestionFollow
